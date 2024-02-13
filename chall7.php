913050442e553a1f3b234502392e460d7cfc6ebb
zakaria <zikokh012@gmail.com>
2024-02-13 02:01:30 +0100

    ajouter

--- chall7.php
+++ chall7.php
@@ -1,21 +1,13 @@
 <?php
-
 function staircase($n) {
-    for ($i = 1; $i <= $n; $i++) {
-        // Print spaces
-        for ($j = $n - $i; $j > 0; $j--) {
+    for ($i=1 ; $i<= $n ; $i++){
+        for($j=$n-$i; $j>0; $j--){
             echo " ";
         }
-
-        // Print # symbols
-        for ($k = 1; $k <= $i; $k++) {
+        for($k=1; $k<=$i; $k++){
             echo "#";
         }
-
-        // Move to the next line
         echo PHP_EOL;
     }
 }
-
-$n = intval(trim(fgets(STDIN)));
-staircase($n);
+?>
